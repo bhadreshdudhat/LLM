9197bda52e2719c648cdaa3c749e64bf49c0b915
BHADRESH\bhadr <bhadreshdudhat@gmail.com>
2025-04-09 11:57:42 -0500

    summarize website using Ollama open source model

--- week1/day1_bd.py
+++ week1/day1_bd.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 # imports
-
 import os
 import requests
 from dotenv import load_dotenv
@@ -13,19 +6,10 @@ from bs4 import BeautifulSoup
 from IPython.display import Markdown, display
 from openai import OpenAI
 
-# If you get an error running this cell, then please head over to the troubleshooting notebook!
-
-
-# In[6]:
-
-
 # Load environment variables in a file called .env
-
 load_dotenv(override=True)
 api_key = os.getenv('OPENAI_API_KEY')
-
 # Check the key
-
 if not api_key:
     print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
 elif not api_key.startswith("sk-proj-"):
@@ -36,32 +20,14 @@ else:
     print("API key found and looks good so far!")
 
 
-# In[9]:
-
 
 openai = OpenAI()
-
-# If this doesn't work, try Kernel menu >> Restart Kernel and Clear Outputs Of All Cells, then run the cells from the top of this notebook down.
-# If it STILL doesn't work (horrors!) then please see the Troubleshooting notebook in this folder for full instructions
-
-
-# In[12]:
-
-
-# To give you a preview -- calling OpenAI with these messages is this easy. Any problems, head over to the Troubleshooting notebook.
-
 message = "Hello, GPT! This is my first ever message to you! Hello!"
 response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
 print(response.choices[0].message.content)
 
 ==============first project===========================
-# In[17]:
 
-
-# A class to represent a Webpage
-# If you're not familiar with Classes, check out the "Intermediate Python" notebook
-
-# Some websites need you to use proper headers when fetching them:
 headers = {
  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
@@ -80,32 +46,17 @@ class Website:
             irrelevant.decompose()
         self.text = soup.body.get_text(separator="\n", strip=True)
 
-
-# In[20]:
-
-
-# Let's try one out. Change the website and add print statements to follow along.
-
 ed = Website("https://edwarddonner.com")
 print(ed.title)
 print(ed.text)
 
-
-# In[23]:
-
-
-# Define our system prompt - you can experiment with this later, changing the last sentence to 'Respond in markdown in Spanish."
 
 system_prompt = "You are an assistant that analyzes the contents of a website \
 and provides a short summary, ignoring text that might be navigation related. \
 Respond in markdown."
 
 
-# In[26]:
-
-
 # A function that writes a User Prompt that asks for summaries of websites:
-
 def user_prompt_for(website):
     user_prompt = f"You are looking at a website titled {website.title}"
     user_prompt += "\nThe contents of this website is as follows; \
@@ -114,14 +65,8 @@ If it includes news or announcements, then summarize these too.\n\n"
     user_prompt += website.text
     return user_prompt
 
-
-# In[29]:
-
-
 print(user_prompt_for(ed))
 
-
-# In[32]:
 
 
 messages = [
@@ -130,16 +75,9 @@ messages = [
 ]
 
 
-# In[35]:
-
-
-# To give you a preview -- calling OpenAI with system and user messages:
-
+# calling OpenAI with system and user messages:
 response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
 print(response.choices[0].message.content)
-
-
-# In[38]:
 
 
 def messages_for(website):
@@ -147,15 +85,9 @@ def messages_for(website):
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_prompt_for(website)}
     ]
-
-
-# In[41]:
-
-
+    
 messages_for(ed)
 
-
-# In[44]:
 
 
 def summarize(url):
@@ -167,35 +99,14 @@ def summarize(url):
     return response.choices[0].message.content
 
 
-# In[47]:
-
-
 summarize("https://edwarddonner.com")
 
-
-# In[51]:
-
-
 # A function to display this nicely in the Jupyter output, using markdown
-
 def display_summary(url):
     summary = summarize(url)
     display(Markdown(summary))
-
-
-# In[53]:
-
-
 display_summary("https://edwarddonner.com")
-
-
-# In[69]:
-
-
 display_summary("https://google.com")
-
-
-# In[ ]:
 
 
 
